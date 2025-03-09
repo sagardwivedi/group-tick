64cf9c7832dd2232558177494a6f696b75e37a57
sagardwivedi <sagar.dw20@gmail.com>
2025-03-09 15:08:25 +0530

    feat: implemented shadcn

--- db/index.ts
+++
@@ -1,5 +0,0 @@
-import { getEnvVar } from "@/lib/env";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-
-export const db = drizzle(getEnvVar("DATABASE_URL"), { schema });
