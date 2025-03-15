dd67230f046d7a4fed70e26b20c22c9c6d24496e
sagardwivedi <sagar.dw20@gmail.com>
2025-03-15 23:17:39 +0530

    Init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    viewTransition: true,
+    typedEnv: true,
+  },
 };
 
 export default nextConfig;
